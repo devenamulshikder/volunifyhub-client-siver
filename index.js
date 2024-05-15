911fcd77f0fc324395a0e3ab508352cbf8a8aec1
devenamulshikder <devenamulshikder@gmail.com>
2024-05-15 15:38:40 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 require("dotenv").config();
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 9000;
 const app = express();
-const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
-};
+// const corsOptions = {
+//   origin: ["http://localhost:5173", "http://localhost:5174"],
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: [
+      // 'http://localhost:5173',
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shu503b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,14 +38,64 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware', token);
+  // no token available
+  return;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+//localhost:5000 and localhost:5173 are treated as same site.  so sameSite value must be strict in development server.  in production sameSite will be none
+// in development server secure will false .  in production secure will be true
+
 async function run() {
   try {
     const volunifyCollection = client.db("volunify").collection("post");
 
     const volunifyRequested = client.db("volunify").collection("requested");
 
+    // jwt token start
+
+    //creating Token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("user for token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+
+    //clearing Token
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("logging out", user);
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
+
+    // jwt token end
+
     app.get("/allPost", async (req, res) => {
-      const result = await volunifyCollection.find().toArray();
+      const result = await volunifyCollection
+        .find()
+        .sort({ date: 1 })
+        .toArray();
       res.send(result);
     });
 
@@ -53,6 +119,16 @@ async function run() {
       const query = { userEmail: email };
       const result = await volunifyCollection.find(query).toArray();
       res.send(result);
+
+      // if (req.user.email !== req.query.email) {
+      //   return res.status(403).send({ message: "forbidden access" });
+      // }
+      // let query = {};
+      // if (req.query?.email) {
+      //   query = { userEmail: req.query.email };
+      // }
+      // const result = await volunifyCollection.find(query).toArray();
+      // res.send(result);
     });
 
     // get requested data for folunifyRequested collection
@@ -77,7 +153,6 @@ async function run() {
       const result = await volunifyCollection.findOne(query);
       res.send(result);
     });
-
 
     // update
     app.put("/updatePage/:id", async (req, res) => {
@@ -118,6 +193,7 @@ async function run() {
     // post2
     app.post("/volunteerRequested", async (req, res) => {
       const requestedVolunteer = req.body;
+
       const result = await volunifyRequested.insertOne(requestedVolunteer);
       res.send(result);
     });
